4b135f35e4be53e7dee999c645c9b2339ba53996
Somayya Avadut <smavadut3@gmail.com>
2025-08-29 00:03:45 +0530

    Added script to run code without load

--- ServerCompiler.cpp
+++ ServerCompiler.cpp
@@ -29,21 +29,6 @@ std::string gen_unique_id() {
     return std::to_string(now);
 }
 
-// URL decode helper
-std::string url_decode(const std::string& s) {
-    std::string out;
-    char ch;
-    for (size_t i = 0; i < s.size(); ++i) {
-        if (s[i] == '+') out += ' ';
-        else if (s[i] == '%' && i + 2 < s.size()) {
-            ch = static_cast<char>(std::strtol(s.substr(i+1,2).c_str(), nullptr, 16));
-            out += ch;
-            i += 2;
-        } else out += s[i];
-    }
-    return out;
-}
-
 int main() {
     crow::SimpleApp app;
 
@@ -58,23 +43,19 @@ int main() {
     // Handle code execution
     CROW_ROUTE(app, "/run").methods("POST"_method)
     ([](const crow::request& req) {
-        std::string body = req.body;
-        auto lang_pos = body.find("language=");
-        auto code_pos = body.find("&code=");
-        if (lang_pos == std::string::npos || code_pos == std::string::npos)
-            return crow::response(400, "Bad Request");
 
-        std::string lang = body.substr(lang_pos + 9, code_pos - (lang_pos + 9));
-        std::string code = body.substr(code_pos + 6);
-        lang = url_decode(lang);
-        code = url_decode(code);
+        auto body = crow::json::load(req.body);
+	std::string language = body["language"].s();
+	std::string code = body["code"].s();
+
+	crow::json::wvalue res;
 
         // Unique temp dir
         std::string id = gen_unique_id();
         fs::create_directories("tmp/" + id);
 
         std::ostringstream result;
-        if (lang == "cpp") {
+        if (language == "cpp") {
             std::string cppFile = "tmp/" + id + "/program.cpp";
             std::ofstream ofs(cppFile);
             ofs << code;
@@ -89,26 +70,20 @@ int main() {
 
             exec_cmd(cmd);
 
-            std::string compile_err = exec_cmd("cat tmp/" + id + "/compile_err.txt");
-            std::string prog_out = exec_cmd("cat tmp/" + id + "/prog_out.txt");
-            std::string prog_err = exec_cmd("cat tmp/" + id + "/prog_err.txt");
+            res["stderr"] = exec_cmd("cat tmp/" + id + "/compile_err.txt");
+            res["stdout"] = exec_cmd("cat tmp/" + id + "/prog_out.txt");
+            res["status"] = exec_cmd("cat tmp/" + id + "/prog_err.txt");
 
-            if (!compile_err.empty()) {
-                result << "<h3>Compilation Error:</h3><pre>" << compile_err << "</pre>";
-            } else {
-                result << "<h3>Program Output:</h3><pre>" << prog_out << "</pre>";
-                if (!prog_err.empty())
-                    result << "<h3>Runtime Error:</h3><pre>" << prog_err << "</pre>";
-            }
         } else {
-            result << "<h3>Only C++ supported for now</h3>";
+	    res["stdout"] = "Only C++ Supported";
+	    res["stderr"] = "";
+	    res["status"] = "";
         }
 
         // Cleanup temp folder
-        fs::remove_all("tmp/" + id);
+        // fs::remove_all("tmp/" + id);
 
-        result << "<a href='/'>Back</a>";
-        return crow::response(result.str());
+	return crow::response(res);
     });
 
     app.port(18080).multithreaded().run();
